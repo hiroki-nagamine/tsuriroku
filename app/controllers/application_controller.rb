ba461a67da730efc533e26a531df69131b32ab0f
lifestream <hiroki.trance.promo@gmail.com>
2019-05-12 14:33:12 +0000

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_form_forgery with: :exception
+  
+  include SessionsHelper
+  
+  private
+  
+  # 事前ログイン認証
+  # index,showをログインしていないユーザーには見せないようにする処理
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end 
+  end
 end
