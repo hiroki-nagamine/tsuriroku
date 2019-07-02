aeb4f5853c2fc0663673d7cec8df04f91a4a176a
lifestream <hiroki.trance.promo@gmail.com>
2019-07-02 14:09:19 +0000

    like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,14 @@ class LikesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    user_ = User.find(params[:like_id])
+    record = Record.find(params[:record_id])
     current_user.like(record)
     flash[:success] = '釣果にいいね！しました'
     redirect_to user
   end
 
   def destroy
-    user = User.find(params[:like_id])
+    record = Record.find(params[:record_id])
     current_user.unlike(record)
     flash[:success] = '釣果のいいね！を解除しました'
     redirect_to user
